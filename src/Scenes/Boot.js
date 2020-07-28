ce15a11edc37d17a64b5d9956f558d872c3705e9
Shubham Singh <shubham88singh@gmail.com>
2020-07-28 06:44:13 +0530

    Addingt the bootscene

---
+++ src/Scenes/Boot.js
@@ -0,0 +1,33 @@
+import 'phaser';
+
+export default class BootScene extends Phaser.Scene {
+  constructor(key) {
+    super(key);
+  }
+
+  preload() {
+    this.load.tilemapTiledJSON('level1', 'assets/tilemaps/level1.json');
+    this.load.tilemapTiledJSON('level2', 'assets/tilemaps/level2.json');
+
+    this.load.spritesheet('RPGpack_sheet', 'assets/images/RPGpack_sheet.png', {
+      frameWidth: 64,
+      frameHeight: 64,
+    });
+
+    this.load.image('portal', 'assets/images/raft.png');
+    this.load.image('title', 'assets/images/title1.png');
+   
+    this.load.image('coin', 'assets/images/coin_01.png');
+    this.load.image('slime', 'assets/images/slime.png');
+    this.load.spritesheet('player', 'assets/images/player.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+    this.load.image('bullet', 'assets/images/ballBlack_04.png');
+    this.load.image('background', 'assets/images/12.png');
+  }
+
+  create() {
+   
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import 'phaser';
 
 import config from './config';
 import Phaser from 'phaser';
+import BootScene from './Scenes/Boot';
 
 class Game extends Phaser.Game {
   constructor() {
     super(config);
+    this.scene.add('Boot', BootScene);
+
+    this.scene.start('Boot');
   }
 }
 
